9d6d93ecadecaf54373d60507cac1bb290fef5be
gongbaodd@MSYS_Inspiron13 <gongbaodd@qq.com>
2020-03-17 17:22:37 +0800

    Add more features

--- cypress/integration/home.feature
+++ cypress/integration/home.feature
@@ -2,3 +2,8 @@ Feature: Read Posts
     Scenario: Home
         Given I am at home page
         Then I should see title "宫不上叔"
+
+        When I click the first Post
+        Then I go to the page with title "宫不上叔"
+
+        When I click the title, I go back to home
--- cypress/integration/home/index.spec.ts
+++ cypress/integration/home/index.spec.ts
@@ -1,4 +1,4 @@
-import { Given, Then } from "cypress-cucumber-preprocessor/steps"
+import { Given, Then, When } from "cypress-cucumber-preprocessor/steps"
 
 Given(/I am at home page/, () => {
     cy.visit('/');
@@ -6,4 +6,21 @@ Given(/I am at home page/, () => {
 
 Then(/I should see title "(.*)"/, value => {
     cy.contains(value).parent('h1');
+});
+
+When(/I click the first Post/, () => {
+    cy.get('article header').first().find('a').click();
+});
+
+Then(/I go to the page with title "(.*)"/, value => {
+    cy.wait(500);
+    cy.contains(value).parent('h1').as('ChildPageTitle');
+});
+
+When(/I click the title, I go back to home/, () => {
+    cy.get('@ChildPageTitle').find('a').click();
+    cy.wait(500);
+    cy.location("pathname").should(pathname => {
+        expect(pathname).to.equal('/');
+    });
 });
