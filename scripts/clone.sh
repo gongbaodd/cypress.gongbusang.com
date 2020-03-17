fd224eb93637e95dbf036b3d597d7b4bd8e159a0
gongbaodd@MSYS_Inspiron13 <gongbaodd@qq.com>
2020-03-18 01:21:13 +0800

    specify ci command

---
+++ scripts/clone.sh
@@ -0,0 +1,7 @@
+# blog
+rm -Rf blog
+git clone https://github.com/gongbaodd/gongbaodd.github.io.git blog
+cd blog
+git checkout master
+cd ..
+
--- scripts/clone_blogs.sh
+++
@@ -1,4 +0,0 @@
-rm -Rf public
-git clone https://github.com/gongbaodd/gongbaodd.github.io.git public
-cd public
-git checkout master
