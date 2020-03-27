50424cbbe5f5d7ce4ab4d0c64236b40f4b0a1079
gongbaodd@MSYS_Inspiron13 <gongbaodd@qq.com>
2020-03-27 16:05:21 +0800

    remove linting warning

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,20 @@
 module.exports = {
-	root: true,
-	env: {
-		es6: true,
-	},
-	parser: "@typescript-eslint/parser",
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: '2018',
-	},
-	extends: [
-		"eslint:recommended",
-		"plugin:@typescript-eslint/eslint-recommended",
-		"plugin:@typescript-eslint/recommended",
-		"plugin:prettier/recommended",
-	],
+  root: true,
+  env: {
+    es6: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: "2018",
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "@typescript-eslint/no-empty-function": ["off"],
+  },
 };
